26b5d110d4f043a950b5ef4896c394528d9d39a3
unknown <naveensartworld@gmail.com>
2019-02-23 22:13:36 +0530

    commit

--- add-customer.php
+++ add-customer.php
@@ -1,184 +1,199 @@
-
 <?php
 session_start();
+
 include('include/config.php');
 if(strlen($_SESSION['alogin'])==0)
-	{	
-header('location:index.php');
+{	
+	header('location:index.php');
 }
 else{
 	
-if(isset($_POST['submit']))
-{
-	$first_name = !empty($_POST['first_name'])?$_POST['first_name']:NULL; 
-	$last_name = !empty($_POST['last_name'])?$_POST['last_name']:NULL;
-	$email = !empty($_POST['email'])?$_POST['email']:NULL;
-	$password = !empty($_POST['password'])?$_POST['password']:NULL;
-	$phone = !empty($_POST['phone'])?$_POST['phone']:NULL;
-	$city = !empty($_POST['city'])?$_POST['city']:NULL;
-	$country = !empty($_POST['country'])?$_POST['country']:NULL;
-	$active=!empty($_POST['active'])?$_POST['active']:NULL;
-	$date = date("Y-m-d H:i:s");
-$sql=mysqli_query($con,"insert into Customers(first_name,last_name,email,password,phone,city,country,active,create_date) values('$first_name','$last_name','$email','$password','$phone','$city','$country','$active', '$date')");
-$_SESSION['msg']="Customer Inserted Successfully !!";
-
-}
-
-
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Admin| Insert Customer</title>
-	<link type="text/css" href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-	<link type="text/css" href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
-	<link type="text/css" href="css/theme.css" rel="stylesheet">
-	<link type="text/css" href="images/icons/css/font-awesome.css" rel="stylesheet">
-	<link type="text/css" href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600' rel='stylesheet'>
-<script src="http://js.nicedit.com/nicEdit-latest.js" type="text/javascript"></script>
-<script type="text/javascript">bkLib.onDomLoaded(nicEditors.allTextAreas);</script>
-
-   <script>
-function getSubcat(val) {
-	$.ajax({
-	type: "POST",
-	url: "get_subcat.php",
-	data:'cat_id='+val,
-	success: function(data){
-		$("#subcategory").html(data);
+	if(isset($_POST['submit']))
+	{
+		$first_name = !empty($_POST['first_name'])?$_POST['first_name']:NULL; 
+		$last_name = !empty($_POST['last_name'])?$_POST['last_name']:NULL;
+		$email = !empty($_POST['email'])?$_POST['email']:NULL;
+		$password = !empty($_POST['password'])?$_POST['password']:NULL;
+		$phone = !empty($_POST['phone'])?$_POST['phone']:NULL;
+		$city = !empty($_POST['city'])?$_POST['city']:NULL;
+		$country = !empty($_POST['country'])?$_POST['country']:NULL;
+		$active=!empty($_POST['active'])?$_POST['active']:1;
+		$deleted=0;
+		$date = date("Y-m-d H:i:s");
+		$sqlQuery =mysqli_query($con,"select row_id from Customers where email = '".$email."'");
+		$updated_by = $_SESSION['id'];
+		$num=mysqli_fetch_array($sqlQuery);
+		if($num>0){
+			$_SESSION['msg']="Customer with this email address already exists !!";
+		}
+		else
+		{
+			$doc_type = 'cust_code';
+			$sequence = getHighKey($doc_type,$con);
+			$sql=mysqli_query($con,"insert into Customers(first_name,last_name,cus_code,email,password,phone,city,country,active,create_date,create_by,last_modify_date,last_modify_by,deleted) values('$first_name','$last_name','$sequence','$email','$password','$phone','$city','$country','$active', '$date','$updated_by','$date','$updated_by','$deleted')");
+			$_SESSION['msg']="Customer Inserted Successfully !!";
+			header("Location:change-password.php");
+		}
 	}
-	});
-}
-function selectCountry(val) {
-$("#search-box").val(val);
-$("#suggesstion-box").hide();
-}
-</script>	
-
-
+?>
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Customers || Add Customer</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php include_once("include/header.php"); ?>
+  
 </head>
 <body>
-<?php include('include/header.php');?>
-
-	<div class="wrapper">
-		<div class="container">
-			<div class="row">
-<?php include('include/sidebar.php');?>				
-			<div class="span9">
-					<div class="content">
-
-						<div class="module">
-							<div class="module-head">
-								<h3>Insert Customer</h3>
-							</div>
-							<div class="module-body">
-
-									<?php if(isset($_POST['submit']))
-{?>
-									<div class="alert alert-success">
-										<button type="button" class="close" data-dismiss="alert">×</button>
-									<strong>Well done!</strong>	<?php echo htmlentities($_SESSION['msg']);?><?php echo htmlentities($_SESSION['msg']="");?>
-									</div>
-<?php } ?>
-
-
-									<?php if(isset($_GET['del']))
-{?>
-									<div class="alert alert-error">
-										<button type="button" class="close" data-dismiss="alert">×</button>
-									<strong>Oh snap!</strong> 	<?php echo htmlentities($_SESSION['delmsg']);?><?php echo htmlentities($_SESSION['delmsg']="");?>
-									</div>
-<?php } ?>
-
-									<br />
-
-			<form class="form-horizontal row-fluid" name="insertCustomer" method="post" enctype="multipart/form-data">
-				<div class="control-group">
-					<label class="control-label" for="basicinput">First Name</label>
-					<div class="controls">
-						<input type="text"    name="first_name"  placeholder="Enter First Name" class="span8 tip" required>
-					</div>
-				</div>
-
-				<div class="control-group">
-					<label class="control-label" for="basicinput">Last Name</label>
-					<div class="controls">
-						<input type="text"    name="last_name"  placeholder="Enter LAst Name" class="span8 tip" required>
-					</div>
-				</div>
-				<div class="control-group">
-					<label class="control-label" for="basicinput">Email</label>
-					<div class="controls">
-						<input type="text"    name="email"  placeholder="Enter Email" class="span8 tip" required>
-					</div>
-				</div>
-
-				<div class="control-group">
-					<label class="control-label" for="basicinput">Password</label>
-					<div class="controls">
-						<input type="text"    name="password"  placeholder="Enter PAssword" class="span8 tip" required>
-					</div>
-				</div>
-
-				<div class="control-group">
-					<label class="control-label" for="basicinput">Phone</label>
-					<div class="controls">
-						<input type="text"    name="phone"  placeholder="Enter Phone" class="span8 tip" required>
-					</div>
-				</div>
-
-				<div class="control-group">
-					<label class="control-label" for="basicinput">City</label>
-					<div class="controls">
-						<input type="text"    name="city"  placeholder="Enter City" class="span8 tip" required>
-					</div>
-				</div>
-
-				<div class="control-group">
-					<label class="control-label" for="basicinput">Country</label>
-					<div class="controls">
-					<input type="text"    name="country"  placeholder="Enter Country" class="span8 tip" required>
-					</div>
-				</div>
-
-
-
-	<div class="control-group">
-		<div class="controls">
-			<button type="submit" name="submit" class="btn">Add Customer</button>
-		</div>
-	</div>
-	</form>
-		</div>
-	</div>
-
-
-	
-						
-						
-					</div><!--/.content-->
-				</div><!--/.span9-->
-			</div>
-		</div><!--/.container-->
-	</div><!--/.wrapper-->
-
-<?php include('include/footer.php');?>
-
-	<script src="scripts/jquery-1.9.1.min.js" type="text/javascript"></script>
-	<script src="scripts/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
-	<script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-	<script src="scripts/flot/jquery.flot.js" type="text/javascript"></script>
-	<script src="scripts/datatables/jquery.dataTables.js"></script>
-	<script>
-		$(document).ready(function() {
-			$('.datatable-1').dataTable();
-			$('.dataTables_paginate').addClass("btn-group datatable-pagination");
-			$('.dataTables_paginate > a').wrapInner('<span />');
-			$('.dataTables_paginate > a:first-child').append('<i class="icon-chevron-left shaded"></i>');
-			$('.dataTables_paginate > a:last-child').append('<i class="icon-chevron-right shaded"></i>');
-		} );
-	</script>
+    <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+    <!-- preloader area start -->
+    <div id="preloader">
+        <div class="loader"></div>
+    </div>
+    <!-- preloader area end -->
+    <!-- page container area start -->
+    <div class="page-container">
+        <!-- sidebar menu area start -->
+        <div class="sidebar-menu">
+            <div class="sidebar-header">
+                <div class="logo">
+                    <a href="index.html"><img src="assets/images/icon/logo.png" alt="logo"></a>
+                </div>
+            </div>
+            <div class="main-menu">
+                <div class="menu-inner">
+                    <?php include("include/sidebar.php"); ?>
+                </div>
+            </div>
+        </div>
+        <!-- sidebar menu area end -->
+        <!-- main content area start -->
+        <div class="main-content">
+            <!-- header area start -->
+            
+            <!-- header area end -->
+            <!-- page title area start -->
+      		<div class="page-title-area">
+                <div class="row align-items-center">
+                    <div class="col-sm-6">
+                        <div class="breadcrumbs-area clearfix" style="padding:30px;">
+                            <h4 class="page-title pull-left">Add Customer</h4>
+                       </div>
+                    </div>
+          
+                </div>
+            </div>
+            <!-- page title area end -->
+            <div class="main-content-inner">
+            	<div class="col-12 mt-5">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h4 class="header-title">Add Customer</h4>
+                                        <?php if(isset($_SESSION['msg']) and $_SESSION['msg']!='') { ?>
+                                        <div class="alert alert-danger" role="alert" style="margin::0 0 10px 0;">
+                                               <?php echo htmlentities($_SESSION['msg']); ?><?php echo htmlentities($_SESSION['msg']="");?>
+                                         </div>
+                                        <?php } ?>
+                                        <?php if(isset($_SESSION['smsg']) and $_SESSION['smsg']!='') { ?>
+                                     	 <div class="alert alert-success" role="alert"  style="margin::0 0 10px 0;">
+                                               <?php echo htmlentities($_SESSION['smsg']); ?><?php echo htmlentities($_SESSION['smsg']="");?>
+                                         </div>
+                                        <?php } ?>
+                                        <form class="needs-validation" novalidate="" autocomplete="off" name="insertCustomer" onSubmit="return valid();" method="post" enctype="multipart/form-data">
+										
+                                            <div class="form-group">
+                                                <label for="fname">First Name</label>
+                                                <input type="text" name="first_name" value="<?php echo $_POST['first_name']; ?>" id="fname" placeholder="Enter First Name" class="form-control" required>
+                                                
+                                            </div>
+                                            <div class="form-group">
+                                                <label class="control-label" for="basicinput">Last Name</label>
+                                                <input type="text"    name="last_name" value="<?php echo $_POST['last_name']; ?>"   placeholder="Enter Last Name" class="form-control" required>
+                                               
+                                            </div>
+                                            <div class="form-group">
+                                                <label class="control-label" for="basicinput">Email</label> 
+                                                <input type="email"    name="email" value="<?php echo $_POST['email']; ?>"   placeholder="Enter Email" class="form-control" required>	
+                                            </div>
+                                             <div class="form-group">
+                                                <label class="control-label" for="basicinput">Password</label>
+                                                 <input type="text" name="prevent_autofill" id="prevent_autofill" value="" style="display:none;" />
+							<input type="password" name="password_fake" id="password_fake" value="" style="display:none;" />
+                                                <input type="Password"    name="password"  placeholder="Enter Password" class="form-control" required>	
+                                            </div>
+                                            <div class="form-group">
+                                                <label class="control-label" for="basicinput">Confirm Password</label>
+                                                <input type="Password"    name="cpassword"  placeholder="Enter Password Again" onBlur="javascript:valid();" class="form-control" required>
+                                                <div class="invalid-feedback" id="invalid_feedback" style="display:none;">Password and Confirm Password Field do not match  !!	</div>	
+                                            </div>
+                                             <div class="form-group">
+                                                <label class="control-label" for="basicinput">Phone</label>
+                                                 <input type="text"    name="phone" value="<?php echo $_POST['phone']; ?>"  placeholder="Enter Phone" class="form-control" required>
+                                            </div>
+                                             <div class="form-group">
+                                                <label class="control-label" for="basicinput">City</label>
+                                                 <input type="text"    name="city" value="<?php echo $_POST['city']; ?>"  placeholder="Enter City" class="form-control" required>
+                                            </div>
+                                              <div class="form-group">
+                                                <label class="control-label" for="basicinput">Country</label>
+                                                <select name ="country" class="form-control" required> 
+                                                	<option value="">Select Country</option>
+                                                	<?php 
+													 $sqlQuery = 'select row_id,country_name from countries where active ="1"';
+													 $result = mysqli_query($con,$sqlQuery);
+													 while($data = mysqli_fetch_array($result))
+													{ ?>
+                                                    	<option value="<?php echo $data['row_id']; ?>" 
+                                                        	<?php 
+																if($data['row_id']==$_POST['country']){
+																	echo 'selected="selected"';	
+																}
+															?>
+                                                       	 ><?php echo $data['country_name']; ?></option>
+                                                    <?php } ?>
+                                                </select>
+                                                
+                                            </div>
+                                            <button type="submit" name="submit" class="btn btn-primary mt-4 pr-4 pl-4">Submit</button>
+                                           
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                            </div>
+       	 </div>
+        <!-- main content area end -->
+        <!-- footer area start-->
+        <footer>
+            <div class="footer-area">
+                <p>© Copyright <?php echo date("Y"); ?>. All right reserved.</p>
+            </div>
+        </footer>
+        <!-- footer area end-->
+    </div>
 </body>
+<?php include_once("include/footer.php"); ?>
+  <script type="text/javascript">
+	function valid()
+	{
+		
+		if(document.insertCustomer.password.value!= document.insertCustomer.cpassword.value)
+		{
+			$("#invalid_feedback").html("Password and Confirm Password Field do not match  !!");
+			$("#invalid_feedback").show();
+			$("#exampleInputPassword2").attr("style","border-color: #dc3545");
+			document.insertCustomer.password.focus();
+			return false;
+		}
+		else{
+			$("#invalid_feedback").hide();
+		}
+		return true;
+	}
+</script>
+</html>
 <?php } ?>
--- include/config.php
+++ include/config.php
@@ -9,4 +9,17 @@ if (mysqli_connect_errno())
 {
  echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
+
+// COMMON FUNCTIONS
+
+
+function getHighKey($doc_type,$con){
+	$sql=mysqli_query($con,"SELECT format,next_num,increaments FROM  sequence where doc_type='".$doc_type."'");
+	$num=mysqli_fetch_array($sql);
+	$nextNum = $num['next_num']+$num['increaments'];
+	mysqli_query($con,"update sequence set next_num='".$nextNum."'  where doc_type='".$doc_type."'");
+	$sequence = $num['format'].$nextNum;
+	return $sequence;
+}
+
 ?>
